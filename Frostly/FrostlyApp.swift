ea2e641c9909ad851dd4073c56309641159ea3f6
Alex G <hi@mavenblueprint.com>
2020-07-20 13:48:42 -0400

    Initial Commit

---
+++ Frostly/FrostlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrostlyApp.swift
+//  Frostly
+//
+//  Created by alexseven on 7/20/20.
+//
+
+import SwiftUI
+
+@main
+struct FrostlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
